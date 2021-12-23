5044b54d9aee1c76d915e48c9e8e97f0a955f52e
Tristan <LeoDog896@hotmail.com>
2021-12-22 21:42:37 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,21 @@
+sudo dnf install qt5-qtbase -y
+wget https://files.multimc.org/downloads/mmc-stable-lin64.tar.gz -O MultiMc.tar.gz
+wget https://avatars2.githubusercontent.com/u/5411890 -O MultiMC.png
+sudo tar -xf MultiMc.tar.gz --strip-components=1
+chmod +x ./MultiMC
+rm MultiMc.tar.gz
+echo "[Desktop Entry]
+Comment=
+Exec=$PWD/MultiMC
+GenericName=Manage Minecraft instances with ease
+Icon=$PWD/MultiMC.png
+Name=MultiMC
+NoDisplay=false
+Path[$e]=
+StartupNotify=true
+Terminal=0
+TerminalOptions=
+Type=Application
+X-KDE-SubstituteUID=false
+X-KDE-Username=
+" >> ~/.local/share/applications/MultiMC.desktop
