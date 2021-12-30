dddb46ffb6977c2ec608068b0d6d8593bc6457fe
Tristan <LeoDog896@hotmail.com>
2021-12-30 16:12:37 -0500

    Update install-fedora.sh

--- install-fedora.sh
+++ install-fedora.sh
@@ -18,12 +18,13 @@ then
   sudo dnf install qt5-qtbase -y
 elif command -v apt &> /dev/null
 then
-  printf -- "Installing libqt5core5a libqt5network5 libqt5gui5, required for MultiMC on this OS\n"
+  printf -- "Installing libqt5core5a libqt5network5 +libqt5gui5, required for MultiMC on this OS\n"
   sudo apt install libqt5core5a libqt5network5 libqt5gui5
 elif command -v zypper &> /dev/null
+then
   printf -- "Installing libqt5-qtbase, required for MultiMC on this OS\n"
   sudo zypper install libqt5-qtbase
-fin
+fi
 
 printf -- "Downloading MultiMC...\n"
 mkdir ~/.local/MultiMC
